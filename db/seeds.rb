5c68c65c94f92dc89d8422a01921fdc009d9f385
Kevin Jolley <jolley427@gmail.comm>
2023-07-06 12:55:00 -0400

    added GPU in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,2 @@
-
+product = Product.new(name: "Graphics Card", price: 1400, image_url: "https://images.evga.com/products/gallery/png/12G-P5-3967-KR_LG_1.png", description: "Awesome graphics card. Suck it AMD.")
+product.save
