5c3074f215156b405bd0cb8b01eb7ec0d186c3fb
Kevin Jolley <jolley427@gmail.comm>
2023-07-07 13:49:33 -0400

    cleaned up code

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,19 +8,4 @@ class ProductsController < ApplicationController
     @product = Product.find_by(id: params["id"])
     render template: "products/show"
   end
-
-  # def first_product
-  #   @product = Product.first
-  #   render template: "products/show"
-  # end
-
-  # def second_product
-  #   @product = Product.second
-  #   render template: "products/show"
-  # end
-
-  # def third_product
-  #   @product = Product.third
-  #   render template: "products/show"
-  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
   get "/all_products" => "products#all_products"
   get "/product/:id" => "products#one_product"
-  # get "/first_product", controller: "products", action: "first_product"
-  # get "/second_product", controller: "products", action: "second_product"
-  # get "/third_product", controller: "products", action: "third_product"
 end
