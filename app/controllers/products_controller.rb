91d21454bfe67117e8e5c09addb3d041ce73eb6c
Kevin Jolley <jolley427@gmail.comm>
2023-07-07 15:18:48 -0400

    updated create action to be dynamic

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,10 +11,10 @@ class ProductsController < ApplicationController
 
   def create
     @product = Product.create(
-      name: "PSU",
-      price: 200,
-      image_url: "https://c1.neweggimages.com/ProductImage/17-151-196-S09.jpg",
-      description: "Fully modular power supply outputting up to 850 watts with 80+ Titanium efficiency.",
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"],
     )
     render :show
   end
