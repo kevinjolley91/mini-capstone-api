cb9d630f6f5a375d5eddc990fbec6de99e88a436
Kevin Jolley <jolley427@gmail.comm>
2023-07-11 15:06:57 -0400

    added is_discounted, tax, total model methods

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+  def is_discounted?
+    result = false
+    result = true if price <= 300
+    result
+  end
+
+  def tax
+    tax = price * 0.09
+    tax
+  end
+
+  def total
+    total = price + tax
+    total
+  end
 end
