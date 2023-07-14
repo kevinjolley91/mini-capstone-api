a7433ee089e9fef07e773bdd4b681b5951f65cee
Kevin Jolley <jolley427@gmail.comm>
2023-07-14 15:41:24 -0400

    updated image

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -8,4 +8,34 @@ class ImagesController < ApplicationController
     @image = Image.find_by(id: params[:id])
     render :show
   end
+
+  def create
+    @image = Image.create(
+      product_id: params[:product_id],
+      url: params[:url],
+    )
+    if @image.save
+      render :show
+    else
+      render json: { errors: @image.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @image = Image.find_by(id: params[:id])
+    @image.update(
+      product_id: params[:id] || @image.product_id,
+      url: params[:url] || @image.url,
+    )
+    if @image.save
+      render :show
+    else
+      render json: { errors: @image.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def delete
+    @image = Image.find_by(id: params[:id])
+    image.destroy
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
   delete "/suppliers/:id" => "suppliers#delete"
   get "/images" => "images#index"
   get "/images/:id" => "images#show"
+  post "/images" => "images#create"
+  patch "/images/:id" => "images#update"
+  delete "/images/:id" => "images#delete"
 end
