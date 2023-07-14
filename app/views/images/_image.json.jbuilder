84e9100d4f3492ae1d64ad52d41d4d1b85eee20e
Kevin Jolley <jolley427@gmail.comm>
2023-07-14 13:47:03 -0400

    adds product_id to images

--- app/views/images/_image.json.jbuilder
+++ app/views/images/_image.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! image, :id, :created_at, :updated_at
+json.extract! image, :id, :product_id, :url
 json.url image_url(image, format: :json)
--- db/migrate/20230714174207_add_url_to_image.rb
+++ db/migrate/20230714174207_add_url_to_image.rb
@@ -1,5 +1,5 @@
 class AddUrlToImage < ActiveRecord::Migration[7.0]
-  def change
-    add_column :images, :url, :text
-  end
+  # def change
+  #   add_column :images, :url, :text
+  # end
 end
