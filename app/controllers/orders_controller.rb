5ae5f589affa8db72626e80d39d0b8bc5fa3bc8d
Kevin Jolley <jolley427@gmail.comm>
2023-07-20 12:19:09 -0400

    updates show and index actions to check if user is logged in

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -16,20 +16,20 @@ class OrdersController < ApplicationController
   end
 
   def show
-    @order = current_user.orders.find_by(id: params[:id])
-    if @order
+    if current_user
+      @order = current_user.orders.find_by(id: params[:id])
       render :show
     else
-      render json: { errors: @order.errors.full_messages }
+      render json: { message: "Please log in to see your order." }, status: :unauthorized
     end
   end
 
   def index
-    @orders = current_user.orders
-    if @order
+    if current_user
+      @orders = current_user.orders.all
       render :index
     else
-      render json: { errors: @order.errors.full_messages }
+      render json: { message: "Please log in to see your orders." }, status: :unauthorized
     end
   end
 end
