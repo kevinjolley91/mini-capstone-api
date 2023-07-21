263ef35b122347209cd0156815ab42bf075d06ed
Kevin Jolley <jolley427@gmail.comm>
2023-07-21 16:27:14 -0400

    updated index method for specific status, broke other stuff

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -11,8 +11,7 @@ class CartedProductsController < ApplicationController
   end
 
   def index
-    @carted_products = current_user.carted_products
+    @carted_products = current_user.carted_products.where(status: "carted")
     render :index
   end
 end
-––––
