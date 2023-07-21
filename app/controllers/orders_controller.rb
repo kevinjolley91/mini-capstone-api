95bcaeff3720b92166c073b1543c53480050028d
Kevin Jolley <jolley427@gmail.comm>
2023-07-21 13:56:52 -0400

    adds association for carted_products model

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,8 +4,6 @@ class OrdersController < ApplicationController
   def create
     @order = Order.new(
       user_id: current_user.id,
-      product_id: params[:product_id],
-      quantity: params[:quantity],
       subtotal: Product.find_by(id: params[:product_id]).price,
       tax: Product.find_by(id: params[:product_id]).tax,
       total: Product.find_by(id: params[:product_id]).total,
