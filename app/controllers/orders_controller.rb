3fa9c82da78c12b275d3281f390fbf747ae1320a
Kevin Jolley <jolley427@gmail.comm>
2023-07-24 17:11:30 -0400

    fixed orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,14 +2,24 @@ class OrdersController < ApplicationController
   before_action :authenticate_user
 
   def create
+    carted_products = current_user.carted_products.where(status: "carted")
+
+    calculated_subtotal = 0
+    carted_products.each do |carted_product|
+      calculated_subtotal += carted_product.quantity * carted_product.product.price
+    end
+    calculated_tax = calculated_subtotal * 0.09
+    calculated_total = calculated_subtotal + calculated_tax
+
     @order = Order.new(
       user_id: current_user.id,
-      subtotal: Product.find_by(id: CartedProduct.product_id).price * CartedProduct.quantity,
-      tax: Product.find_by(id: CartedProduct.product_id).tax * CartedProduct.quantity,
-      total: Product.find_byproduct_(id: CartedProduct.product_id).total * CartedProduct.quantity,
+      subtotal: calculated_subtotal,
+      tax: calculated_tax,
+      total: calculated_total,
     )
     if @order.save
       render json: { message: "Order accepted." }
+      carted_products.update_all(status: "purchased", order_id: @order_id)
     else
       render json: { errors: @order.errors.full_messages }
     end
