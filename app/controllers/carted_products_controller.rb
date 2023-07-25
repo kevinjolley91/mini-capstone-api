c856d6d7d9b19bf97dc248fc3873027e72ae153e
Kevin Jolley <jolley427@gmail.comm>
2023-07-25 12:46:57 -0400

    fixed carted products controller

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -13,4 +13,10 @@ class CartedProductsController < ApplicationController
     @carted_products = current_user.carted_products.where(status: "carted")
     render :index
   end
+
+  def destroy
+    carted_product = current_user.carted_products.find_by(id: params[:id], status: "carted")
+    carted_product.update(status: "removed")
+    render json: { message: "product successfully removed from cart 😈" }
+  end
 end
--- app/views/orders/_order.json.jbuilder
+++ app/views/orders/_order.json.jbuilder
@@ -1,1 +1,1 @@
-json.extract! order, :id, :user, :subtotal, :tax, :total, :created_at, :updated_at
+json.extract! order, :id, :user, :subtotal, :tax, :total,  :created_at, :updated_at
