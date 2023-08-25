269902cccc8507de6479a6e79f1c62137ce6524f
Kevin Jolley <jolley427@gmail.comm>
2023-08-25 13:43:43 -0400

    reset db again

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   has_many :orders
   has_many :carted_products
   has_secure_password
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true
 end
